d29fa0861be7d2ff059d0b25fb4a1750dd8b707e
Kavia AI <production@kavia.ai>
2026-03-05 18:01:44 +0000

    Checkpoint 1 - cgca4edb1a

---
+++ .init/.linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/unified-notes-manager-329624-329640/nextjs_frontend
+npm run lint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
